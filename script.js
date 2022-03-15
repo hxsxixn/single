e44551036955c12e65802d4cd64e58bf417602f0
hxsxixn <100697095+hxsxixn@users.noreply.github.com>
2022-03-15 10:59:01 +0800

    fix swiper

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var swiper = new Swiper('.swiper',{
-  pagination: {el:'.swiper-pagination',},
+  pagination: {el:'.swiper-pagination'},
   loop: true,
   slidesPerview: 'auto',
   speed: 1000,
   autoplay:{
     delay: 2000,
-  }
+    disableOnInteraction: false,
+  },
 })
